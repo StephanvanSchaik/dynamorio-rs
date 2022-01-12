95cc1448dd4bfaeba31b7b7eb0e053e834ea622b
S.J.R. van Schaik <stephan@synkhronix.com>
2022-01-12 12:08:03 -0500

    implement Symbols

---
+++ dynamorio-rs/src/syms.rs
@@ -0,0 +1,53 @@
+use dynamorio_sys::*;
+use std::ffi::CStr;
+
+pub struct Symbols;
+
+impl Symbols {
+    pub fn new() -> Self {
+        unsafe {
+            drsym_init(0);
+        }
+
+        Self
+    }
+
+    pub fn lookup_address(
+        &self,
+        path: &str,
+        offset: usize,
+        flags: drsym_flags_t,
+    ) -> Option<String> {
+        let mut name = vec![0i8; 256];
+        let mut file_name = vec![0i8; 256];
+        let mut sym_info: drsym_info_t = unsafe {
+            std::mem::zeroed()
+        };
+
+        sym_info.struct_size = std::mem::size_of::<drsym_info_t>();
+        sym_info.name = name.as_mut_ptr();
+        sym_info.name_size = name.len() as _;
+        sym_info.file = file_name.as_mut_ptr();
+        sym_info.file_size = file_name.len() as _;
+    
+        let result = unsafe {
+            drsym_lookup_address(
+                path.as_ptr() as _,
+                offset,
+                &mut sym_info,
+                flags.0,
+            )
+        };
+
+        if result != drsym_error_t::DRSYM_SUCCESS {
+            return None;
+        }
+
+        unsafe {
+            CStr::from_ptr(sym_info.name)
+                 .to_str()
+                 .map(|s| s.to_owned())
+                 .ok()
+        }
+    }
+}
