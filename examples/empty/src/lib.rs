80c99bfc6c8b7690694668430652960b6255506d
S.J.R. van Schaik <stephan@synkhronix.com>
2022-01-11 19:36:02 -0500

    examples: mark arguments to client_main as unused

--- examples/empty/src/lib.rs
+++ examples/empty/src/lib.rs
@@ -4,7 +4,7 @@ fn event_exit() {
 }
 
 #[no_mangle]
-fn client_main(id: ClientId, args: &[&str]) {
+fn client_main(_id: ClientId, _args: &[&str]) {
     set_client_name("empty", "https://github.com/StephanvanSchaik/dynamorio-rs/issues");
 
     register_exit_event(event_exit);
