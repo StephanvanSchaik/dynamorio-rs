b5e1ebdc0e47bce0aeb501720855bdf7e1d1a8ca
S.J.R. van Schaik <stephan@synkhronix.com>
2022-01-12 16:42:55 -0500

    implement module, thread and syscall events using user_data to pass the handlers

--- dynamorio-rs/src/mgr.rs
+++ dynamorio-rs/src/mgr.rs
@@ -4,22 +4,68 @@ use dynamorio_sys::*;
 
 pub use dynamorio_sys::dr_emit_flags_t;
 
-static MODULE_LOAD_HANDLER: Atomic<Option<fn(&mut Context, &ModuleData, bool) -> ()>> = Atomic::new(None);
 static BB_ANALYSIS_HANDLER: Atomic<Option<fn(&mut Context, &InstructionList, bool, bool) -> dr_emit_flags_t>> = Atomic::new(None);
 static BB_INSTRUMENTATION_HANDLER: Atomic<Option<fn(&mut Context, &mut InstructionList, &Instruction, bool, bool) -> dr_emit_flags_t>> = Atomic::new(None);
+
+extern "C" fn before_syscall_event(
+    context: *mut std::ffi::c_void,
+    sysnum: i32,
+    user_data: *mut std::ffi::c_void,
+) -> i8 {
+    let mut context = Context::from_raw(context);
+    let func = unsafe {
+        std::mem::transmute::<*mut std::ffi::c_void, fn(&mut Context, i32) -> bool>(user_data)
+    };
+
+    let result = func(&mut context, sysnum) as i8;
+
+    result
+}
+
+extern "C" fn after_syscall_event(
+    context: *mut std::ffi::c_void,
+    sysnum: i32,
+    user_data: *mut std::ffi::c_void,
+) {
+    let mut context = Context::from_raw(context);
+    let func = unsafe {
+        std::mem::transmute::<*mut std::ffi::c_void, fn(&mut Context, i32) -> ()>(user_data)
+    };
+
+    func(&mut context, sysnum);
+}
 
 extern "C" fn module_load_event(
     context: *mut std::ffi::c_void,
     module: *const module_data_t,
     loaded: std::os::raw::c_char,
+    user_data: *mut std::ffi::c_void,
 ) {
     let module = ModuleData::from_raw(module as _);
     let loaded = loaded != 0;
     let mut context = Context::from_raw(context);
+    let func = unsafe {
+        std::mem::transmute::<*mut std::ffi::c_void, fn(&mut Context, &ModuleData, bool) -> ()>(user_data)
+    };
 
-    if let Some(handler) = MODULE_LOAD_HANDLER.load(Ordering::Relaxed) {
-        handler(&mut context, &module, loaded);
-    }
+    func(&mut context, &module, loaded);
+
+    std::mem::forget(context);
+    std::mem::forget(module);
+}
+
+extern "C" fn module_unload_event(
+    context: *mut std::ffi::c_void,
+    module: *const module_data_t,
+    user_data: *mut std::ffi::c_void,
+) {
+    let module = ModuleData::from_raw(module as _);
+    let mut context = Context::from_raw(context);
+    let func = unsafe {
+        std::mem::transmute::<*mut std::ffi::c_void, fn(&mut Context, &ModuleData) -> ()>(user_data)
+    };
+
+    func(&mut context, &module);
 
     std::mem::forget(context);
     std::mem::forget(module);
@@ -76,6 +122,30 @@ extern "C" fn bb_instrumentation_event(
     flags
 }
 
+extern "C" fn thread_init_event(
+    context: *mut std::ffi::c_void,
+    user_data: *mut std::ffi::c_void,
+) {
+    let mut context = Context::from_raw(context);
+    let func = unsafe {
+        std::mem::transmute::<*mut std::ffi::c_void, fn(&mut Context) -> ()>(user_data)
+    };
+
+    func(&mut context);
+}
+
+extern "C" fn thread_exit_event(
+    context: *mut std::ffi::c_void,
+    user_data: *mut std::ffi::c_void,
+) {
+    let mut context = Context::from_raw(context);
+    let func = unsafe {
+        std::mem::transmute::<*mut std::ffi::c_void, fn(&mut Context) -> ()>(user_data)
+    };
+
+    func(&mut context);
+}
+
 pub struct Manager;
 
 impl Manager {
@@ -85,6 +155,48 @@ impl Manager {
         }
 
         Self
+    }
+
+    #[cfg(target_os = "windows")]
+    pub fn decode_sysnum_from_wrapper(
+        &self,
+        entry: usize,
+    ) -> Option<u32> {
+        let result = unsafe {
+            drmgr_decode_sysnum_from_wrapper(entry)
+        };
+
+        if result < 0 {
+            return None;
+        }
+
+        Some(result as u32)
+    }
+
+    pub fn register_before_syscall_event(
+        &self,
+        func: fn(&mut Context, i32) -> bool,
+    ) {
+        unsafe {
+            drmgr_register_pre_syscall_event_user_data(
+                Some(before_syscall_event),
+                std::ptr::null_mut(),
+                func as *mut std::ffi::c_void,
+            );
+        }
+    }
+
+    pub fn register_after_syscall_event(
+        &self,
+        func: fn(&mut Context, i32) -> (),
+    ) {
+        unsafe {
+            drmgr_register_post_syscall_event_user_data(
+                Some(after_syscall_event),
+                std::ptr::null_mut(),
+                func as *mut std::ffi::c_void,
+            );
+        }
     }
 
     pub fn register_bb_instrumentation_event(
@@ -114,10 +226,51 @@ impl Manager {
         &self,
         func: fn(&mut Context, &ModuleData, bool) -> (),
     ) {
-        MODULE_LOAD_HANDLER.store(Some(func), Ordering::Relaxed);
-
         unsafe {
-            drmgr_register_module_load_event(Some(module_load_event));
+            drmgr_register_module_load_event_user_data(
+                Some(module_load_event),
+                std::ptr::null_mut(),
+                func as *mut std::ffi::c_void,
+            );
+        }
+    }
+
+    pub fn register_module_unload_event(
+        &self,
+        func: fn(&mut Context, &ModuleData) -> (),
+    ) {
+        unsafe {
+            drmgr_register_module_unload_event_user_data(
+                Some(module_unload_event),
+                std::ptr::null_mut(),
+                func as *mut std::ffi::c_void,
+            );
+        }
+    }
+
+    pub fn register_thread_init_event(
+        &self,
+        func: fn(&mut Context) -> (),
+    ) {
+        unsafe {
+            drmgr_register_thread_init_event_user_data(
+                Some(thread_init_event),
+                std::ptr::null_mut(),
+                func as *mut std::ffi::c_void,
+            );
+        }
+    }
+
+    pub fn register_thread_exit_event(
+        &self,
+        func: fn(&mut Context) -> (),
+    ) {
+        unsafe {
+            drmgr_register_thread_exit_event_user_data(
+                Some(thread_exit_event),
+                std::ptr::null_mut(),
+                func as *mut std::ffi::c_void,
+            );
         }
     }
 }
