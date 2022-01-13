e8eb9625e75d5dc98fc2330f4dfccdd577cf1228
S.J.R. van Schaik <stephan@synkhronix.com>
2022-01-13 01:30:54 -0500

    examples: strace: handle arguments and return value

--- examples/strace/src/lib.rs
+++ examples/strace/src/lib.rs
@@ -4,11 +4,28 @@ use syscalls::Sysno;
 fn before_syscall_event(context: &mut BeforeSyscallContext, sysnum: i32) -> bool {
     let sysno = Sysno::from(sysnum);
 
-    println!("{}", sysno.name());
+    let arguments = sysno.arguments()
+        .map(|arguments| {
+            arguments
+                .iter()
+                .enumerate()
+                .map(|(i, _argument)| {
+                    let param = unsafe { context.param(i) };
+
+                    format!("0x{:x}", param)
+                })
+                .collect::<Vec<String>>()
+                .join(", ")
+        })
+        .unwrap_or("???".to_string());
+
+    print!("{}({:?})", sysno.name(), arguments);
+
     true
 }
 
-fn event_exit() {
+fn after_syscall_event(context: &mut AfterSyscallContext, _sysnum: i32) {
+    println!(" = 0x{:x}", context.get_result());
 }
 
 #[no_mangle]
@@ -17,7 +34,9 @@ fn client_main(_id: ClientId, _args: &[&str]) {
     set_client_name("strace", "https://github.com/StephanvanSchaik/dynamorio-rs/issues");
 
     manager.register_before_syscall_event(before_syscall_event);
-    register_exit_event(event_exit);
+    manager.register_after_syscall_event(after_syscall_event);
 
-    println!("Hello, world!");
+    // Make sure the system call definitions have been initialized.
+    let sysno = Sysno::read;
+    sysno.arguments();
 }
