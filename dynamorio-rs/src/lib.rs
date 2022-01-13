755e20648ec3f6275a10d26e67ecd32b6abd9038
S.J.R. van Schaik <stephan@synkhronix.com>
2022-01-12 19:07:57 -0500

    implement register_fork_event()

--- dynamorio-rs/src/lib.rs
+++ dynamorio-rs/src/lib.rs
@@ -48,10 +48,19 @@ pub static _USES_DR_VERSION_: std::os::raw::c_int = dynamorio_sys::_USES_DR_VERS
 pub static _DR_CLIENT_AVX512_CODE_IN_USE: std::os::raw::c_char = dynamorio_sys::_DR_CLIENT_AVX512_CODE_IN_USE_;
 
 static EXIT_HANDLER: Atomic<Option<fn() -> ()>> = Atomic::new(None);
+static FORK_HANDLER: Atomic<Option<fn(&mut Context) -> ()>> = Atomic::new(None);
 
-extern "C" fn event_exit() {
+extern "C" fn exit_event() {
     if let Some(handler) = EXIT_HANDLER.load(Ordering::Relaxed) {
         handler()
+    }
+}
+
+extern "C" fn fork_event(context: *mut std::ffi::c_void) {
+    let mut context = Context::from_raw(context);
+
+    if let Some(handler) = FORK_HANDLER.load(Ordering::Relaxed) {
+        handler(&mut context)
     }
 }
 
@@ -61,8 +70,21 @@ pub fn register_exit_event(func: fn() -> ()) {
     EXIT_HANDLER.store(Some(func), Ordering::Relaxed);
 
     unsafe {
-        dr_register_exit_event(Some(event_exit));
+        dr_register_exit_event(Some(exit_event));
     }
+}
+
+#[cfg(unix)]
+pub fn register_fork_event(func: fn(&mut Context) -> ()) {
+    FORK_HANDLER.store(Some(func), Ordering::Relaxed);
+
+    unsafe {
+        dr_register_fork_init_event(Some(fork_event));
+    }
+}
+
+#[cfg(not(unix))]
+pub fn register_fork_event(func: fn(&mut Context) -> ()) {
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
