c045a655038b0a47e9b1174223ac67895b34ba8f
S.J.R. van Schaik <stephan@synkhronix.com>
2022-01-21 05:16:17 -0500

    dynamorio-rs: implement ModuleHandler

--- dynamorio-rs/src/manager.rs
+++ dynamorio-rs/src/manager.rs
@@ -59,7 +59,32 @@ extern "C" fn after_syscall_event<T: SyscallHandler>(
     }
 }
 
-extern "C" fn module_load_event(
+pub trait ModuleHandler {
+    fn load_module(&mut self, context: &mut Context, module: &ModuleData, loaded: bool);
+    fn unload_module(&mut self, context: &mut Context, module: &ModuleData);
+}
+
+pub struct RegisteredModuleHandler<T: ModuleHandler> {
+    _handler: Arc<Mutex<T>>,
+}
+
+impl<T: ModuleHandler> Drop for RegisteredModuleHandler<T> {
+    fn drop(&mut self) {
+        unsafe {
+            drmgr_unregister_module_load_event_user_data(
+                Some(module_load_event::<T>),
+            );
+        }
+
+        unsafe {
+            drmgr_unregister_module_unload_event_user_data(
+                Some(module_unload_event::<T>),
+            );
+        }
+    }
+}
+
+extern "C" fn module_load_event<T: ModuleHandler>(
     context: *mut core::ffi::c_void,
     module: *const module_data_t,
     loaded: libc::c_char,
@@ -68,28 +93,28 @@ extern "C" fn module_load_event(
     let module = ModuleData::from_raw(module as _);
     let loaded = loaded != 0;
     let mut context = Context::from_raw(context);
-    let func = unsafe {
-        core::mem::transmute::<*mut core::ffi::c_void, fn(&mut Context, &ModuleData, bool) -> ()>(user_data)
-    };
+    let handler = unsafe { &*(user_data as *mut Mutex<T>) };
 
-    func(&mut context, &module, loaded);
+    if let Ok(mut handler) = handler.lock() {
+        handler.load_module(&mut context, &module, loaded);
+    }
 
     core::mem::forget(context);
     core::mem::forget(module);
 }
 
-extern "C" fn module_unload_event(
+extern "C" fn module_unload_event<T: ModuleHandler>(
     context: *mut core::ffi::c_void,
     module: *const module_data_t,
     user_data: *mut core::ffi::c_void,
 ) {
     let module = ModuleData::from_raw(module as _);
     let mut context = Context::from_raw(context);
-    let func = unsafe {
-        core::mem::transmute::<*mut core::ffi::c_void, fn(&mut Context, &ModuleData) -> ()>(user_data)
-    };
+    let handler = unsafe { &*(user_data as *mut Mutex<T>) };
 
-    func(&mut context, &module);
+    if let Ok(mut handler) = handler.lock() {
+        handler.unload_module(&mut context, &module);
+    }
 
     core::mem::forget(context);
     core::mem::forget(module);
@@ -245,29 +270,28 @@ impl Manager {
         }
     }
 
-    pub fn register_module_load_event(
+    pub fn register_module_handler<T: ModuleHandler>(
         &self,
-        func: fn(&mut Context, &ModuleData, bool) -> (),
-    ) {
+        handler: &Arc<Mutex<T>>,
+    ) -> RegisteredModuleHandler<T> {
         unsafe {
             drmgr_register_module_load_event_user_data(
-                Some(module_load_event),
+                Some(module_load_event::<T>),
                 core::ptr::null_mut(),
-                func as *mut core::ffi::c_void,
+                Arc::as_ptr(&handler) as *mut core::ffi::c_void,
             );
         }
-    }
 
-    pub fn register_module_unload_event(
-        &self,
-        func: fn(&mut Context, &ModuleData) -> (),
-    ) {
         unsafe {
             drmgr_register_module_unload_event_user_data(
-                Some(module_unload_event),
+                Some(module_unload_event::<T>),
                 core::ptr::null_mut(),
-                func as *mut core::ffi::c_void,
+                Arc::as_ptr(&handler) as *mut core::ffi::c_void,
             );
+        }
+
+        RegisteredModuleHandler {
+            _handler: Arc::clone(&handler),
         }
     }
 
