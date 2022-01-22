580aaee428b8bd6e33acb6e0772860c618b11705
S.J.R. van Schaik <stephan@synkhronix.com>
2022-01-22 14:16:30 -0500

    dynamorio-rs: generate thunk to attach user_data and implement ExitHandler

--- dynamorio-rs/src/event.rs
+++ dynamorio-rs/src/event.rs
@@ -1,15 +1,13 @@
 use atomic::{Atomic, Ordering};
 use crate::{ClientId, Context};
+use drstd::sync::{Arc, Mutex};
 use dynamorio_sys::*;
 
-static EXIT_HANDLER: Atomic<Option<fn() -> ()>> = Atomic::new(None);
 static FORK_HANDLER: Atomic<Option<fn(&mut Context) -> ()>> = Atomic::new(None);
 static NUDGE_HANDLER: Atomic<Option<fn(&mut Context, u64) -> ()>> = Atomic::new(None);
 
-extern "C" fn exit_event() {
-    if let Some(handler) = EXIT_HANDLER.load(Ordering::Relaxed) {
-        handler()
-    }
+pub trait ExitHandler {
+    fn exit(&mut self);
 }
 
 extern "C" fn fork_event(context: *mut core::ffi::c_void) {
@@ -28,13 +26,71 @@ extern "C" fn nudge_event(context: *mut core::ffi::c_void, argument: u64) {
     }
 }
 
+unsafe extern "C" fn exit_wrapper<T: ExitHandler>(
+    handler: &Mutex<T>,
+) {
+    if let Ok(mut handler) = handler.lock() {
+        handler.exit()
+    }
+}
+
+pub struct RegisteredExitHandler<T: ExitHandler> {
+    _handler: Arc<Mutex<T>>,
+    func: extern "C" fn(),
+    closure: *mut core::ffi::c_void,
+}
+
+unsafe impl<T: ExitHandler> Send for RegisteredExitHandler<T> {}
+unsafe impl<T: ExitHandler> Sync for RegisteredExitHandler<T> {}
+
+impl<T: ExitHandler> Drop for RegisteredExitHandler<T> {
+    fn drop(&mut self) {
+        unsafe {
+            dr_unregister_exit_event(Some(self.func));
+        }
+
+        unsafe {
+            dr_nonheap_free(self.closure, 32);
+        }
+    }
+}
+
 /// Registers a callback function for the process exit event. DynamoRIO calls `func` when the
 /// process exits.
-pub fn register_exit_event(func: fn() -> ()) {
-    EXIT_HANDLER.store(Some(func), Ordering::Relaxed);
+pub fn register_exit_handler<T: ExitHandler>(handler: &Arc<Mutex<T>>) -> RegisteredExitHandler<T> {
+    let closure = unsafe {
+        dr_nonheap_alloc(32, DR_MEMPROT_READ | DR_MEMPROT_WRITE | DR_MEMPROT_EXEC)
+    };
+
+    let storage: &mut [u8] = unsafe {
+        core::slice::from_raw_parts_mut(closure as *mut u8, 32)
+    };
+    let code = &[
+        0x48, 0x8b, 0x3d, 0xe9, 0xff, 0xff, 0xff,
+        0xff, 0x25, 0xeb, 0xff, 0xff, 0xff,
+    ];
+
+    storage[16..16 + 13].copy_from_slice(code);
+
+    let storage: &mut [u64] = unsafe {
+        core::slice::from_raw_parts_mut(closure as *mut u64, 2)
+    };
+
+    storage[0] = Arc::as_ptr(&handler) as u64;
+    storage[1] = exit_wrapper::<T> as u64;
+
+    let func: extern "C" fn() = unsafe {
+        core::mem::transmute((closure as *mut u8).add(16))
+    };
 
     unsafe {
-        dr_register_exit_event(Some(exit_event));
+        dr_register_exit_event(Some(func));
+    }
+
+    RegisteredExitHandler {
+        _handler: Arc::clone(&handler),
+        func,
+        closure,
     }
 }
 
